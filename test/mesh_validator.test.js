62f11f5aa4b658686c3090802195fd2e01fbb97f
Arvid Lindenau <alin@num42.de>
2022-02-06 23:51:22 +0100

    add validation erros, add more validation tests

--- test/mesh_validator.test.js
+++ test/mesh_validator.test.js
@@ -1,15 +1,26 @@
-const valid_json = require("./test_data/mesh.json");
-const invalid_json = {}
+const valid_mesh = require('./test_data/mesh.json');
+const valid_mesh_10000 = require('./test_data/mesh_x_sin_cos_10000.json');
+const valid_mesh_20000 = require('./test_data/mesh_x_sin_cos_20000.json');
 
-let {validator} = require('../lib/mesh_validator.js')
+const invalid_mesh_elements = require('./test_data/invalid_mesh_elements.json');
+const invalid_mesh_nodes = require('./test_data/invalid_mesh_nodes.json');
+const invalid_mesh_values = require('./test_data/invalid_mesh_values.json');
 
-describe('View Spot Finder', () => {
+const {validator} = require('../lib/mesh_validator.js')
 
-    it('validates JSON mesh', () => {
-        expect(validator(valid_json)).toBe(true)
-        expect(validator(invalid_json)).toBe(false)
-    })
-    
+describe('Mesh Validatior', () => {
 
+  it('validates JSON mesh correctly', () => {
+    expect(validator(valid_mesh)).toBe(true)
+    expect(validator(valid_mesh_10000)).toBe(true)
+    expect(validator(valid_mesh_20000)).toBe(true)
+
+  })
+
+  it('throws correct errors', () => {
+    expect(() => { validator(invalid_mesh_elements) }).toThrowError(/Invalid 'element'/)
+    expect(() => { validator(invalid_mesh_nodes) }).toThrowError(/Invalid 'node'/)
+    expect(() => { validator(invalid_mesh_values) }).toThrowError(/Invalid 'value'/)
+  })
     
 })
