1bba7c92bfb05bcae9541a9525edaf8878593a87
Arvid Lindenau <alin@num42.de>
2022-02-08 02:29:52 +0100

    finish serverless handler and update README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # view-spot-finder-node
 Case Coding Challenge – Mapping Algorithm
+
+
+## Instructions
+
+Run on command line:
+
+```sh
+node viewSpot.js <path/to/mesh/file.json> <number of view spots>
+```
+
+Or run as lambda function (with test data): 
+
+```sh 
+serverless invoke -f viewSpotFinder -p test/test_data/mesh.json -l
+```
+
+or locally
+
+```sh 
+serverless invoke local -f viewSpotFinder -p test/test_data/mesh.json -l
+```
--- handler.js
+++ handler.js
@@ -1,15 +1,20 @@
 'use strict';
 
-module.exports.viewSpot = async (event) => {
+const {viewSpotFinder} = require('./lib/viewSpotFinder.js')
+const {meshValidator} = require('./lib/meshValidator.js')
+
+module.exports.viewSpotFinder = async (event) => {
+  try { 
+    meshValidator(event) 
+  }
+  catch(e) {
+    console.error(e)
+  }
+
+  const viewSpots = viewSpotFinder(event)
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'This is where the result should be displayed',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(viewSpots),
   };
 };
